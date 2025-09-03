fcf46f26294bce123355a9b9a207821eafd8e052
Pravin Suresh <selfstudy210619@gmail.com>
2025-09-04 00:08:39 +0530

    ProductDetails

---
+++ tests/components/ProductDetail.test.tsx
@@ -0,0 +1,16 @@
+import { render, screen } from "@testing-library/react";
+import ProductDetail from "../../src/components/ProductDetail";
+import { products } from "../mocks/data";
+
+describe("Product Detail", () => {
+	it("should render list of products", async () => {
+		render(<ProductDetail productId={1} />);
+
+		expect(
+			await screen.findByText(new RegExp(products[0].name))
+		).toBeInTheDocument();
+		expect(
+			await screen.findByText(new RegExp(products[0].price.toString()))
+		).toBeInTheDocument();
+	});
+});
---
+++ tests/mocks/data.ts
@@ -0,0 +1,5 @@
+export const products = [
+            {id:1, name:'Product1', price: 10},
+            {id:1, name:'Product2', price: 12},
+            {id:1, name:'Product3', price: 11},
+        ]
